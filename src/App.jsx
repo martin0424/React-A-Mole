6764127a44140623b0b01ebc73034524c22c3665
martin0424 <h.newland424@gmail.com>
2022-07-25 14:50:12 -0400

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,32 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import MoleContainer from './Components/MoleContainer'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const createMoleHill = () => {
+    let hills = []
+    for(let i = 0; i < 0; i++) {
+      hills.push(<MoleContainer key={i} setScore={setScore} score={score} />)
+    }
+    return (
+      <div>
+        { hills }
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1>React-A-Mole</h1>
+      {score}
+      {createMoleHill()}
     </div>
-  )
+  );
+
 }
+
+ 
 
 export default App
