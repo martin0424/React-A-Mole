f054027a6688974f500698e8fbf34a350a52014c
martin0424 <h.newland424@gmail.com>
2022-07-25 15:11:14 -0400

    updated application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import reportWedVitals from './reportWebVitals'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWedVitals(); 
+
+
